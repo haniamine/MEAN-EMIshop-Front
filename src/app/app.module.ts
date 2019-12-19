250d8542d9cc652ac8558658b1e43b483b974e28
oussama <oussamasebbani@hotmail.fr>
2019-12-19 15:28:42 +0100

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {ProductItemComponent} from './product-item/product-item.component';
 import {FormsModule} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import  {MatCardModule} from "@angular/material";
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { EditproductComponent } from './editproduct/editproduct.component';
@@ -36,6 +37,7 @@ const appRoutes: Routes = [
     FormsModule,
     MatSliderModule,
     MatButtonModule,
+    MatCardModule,
     RouterModule.forRoot(
       appRoutes,
       {enableTracing: true} // <-- debugging purposes only
