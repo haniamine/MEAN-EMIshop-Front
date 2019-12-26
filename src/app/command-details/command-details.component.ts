7d62168d18d4e74f98e7d34e0d6a2d774db17c17
oussama <oussamasebbani@hotmail.fr>
2019-12-26 12:59:17 +0100

    tyr

--- src/app/command-details/command-details.component.ts
+++ src/app/command-details/command-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommandeService } from '../shared/services/commande.service';
 import { Route } from '@angular/compiler/src/core';
 import { ActivatedRoute } from '@angular/router';
+import Product from '../shared/model/Product';
+import {Commande} from '../shared/model/commande';
+import {ProductService} from '../shared/services/product.service';
 
 @Component({
   selector: 'app-command-details',
@@ -10,11 +13,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CommandDetailsComponent implements OnInit {
 
-  constructor(private commandService: CommandeService, private route: ActivatedRoute) { }
-
+  constructor(private commandService: CommandeService, private route: ActivatedRoute, private productService: ProductService) { }
+  commande: Commande;
+  products: Product[];
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id');
     this.commandService.getDetails(id);
+    this.commande = this.commandService.selectedCommand;
+    this.commande.products.map(c => {
+      this.products.push();
+    });
+    console.log(this.products);
   }
+
 
 }
